405c088103bb1a4934f304caed43f1d04beec9f7
YUKI KASAI <kasayu0527@outlook.jp>
2024-05-16 01:30:22 +0900

    .envでトークンを隠した

---
+++ src/config.py
@@ -0,0 +1,7 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+
+YOUR_CHANNEL_SECRET = os.getenv('YOUR_CHANNEL_SECRET')
+YOUR_CHANNEL_ACCESS_TOKEN = os.getenv('YOUR_CHANNEL_ACCESS_TOKEN')
--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import os
+import config
+
 from flask import Flask, abort, request
 from linebot.v3.webhook import (
     WebhookHandler
@@ -18,12 +20,13 @@ from linebot.v3.webhooks import (
     TextMessageContent
 )
 
+secret_key = config.YOUR_CHANNEL_SECRET
+access_key = config.YOUR_CHANNEL_ACCESS_TOKEN
 
 app = Flask(__name__)
 
-handler = WebhookHandler('*******')
-configuration = Configuration(access_token='*******')
-
+handler = WebhookHandler(secret_key)
+configuration = Configuration(access_token=access_key)
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -47,15 +50,24 @@ def callback():
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
     with ApiClient(configuration) as api_client:
+        # ユーザーIDを取得
+        user_id = event.source.user_id
+        print(f"User ID: {user_id}")
+
+        #特定のidの時に条件分岐して返答する
+        if user_id == 'U5617c76a1a1e14c3b9dd7bf131246d52':
+
         #相手の送信した内容で条件分岐して回答を変数に代入
-        if event.message.text == 'グー':
-            msg = 'パー'
-        elif event.message.text == 'チョキ':
-            msg = 'グー'
-        elif event.message.text == 'パー':
-            msg = 'チョキ'
+            if event.message.text == 'グー':
+                msg = 'パー'
+            elif event.message.text == 'チョキ':
+                msg = 'グー'
+            elif event.message.text == 'パー':
+                msg = 'チョキ'
+            else:
+                msg = 'ごめんね。\nまだ他のメッセージには対応してないよ'
         else:
-            msg = 'ごめんね。\nまだ他のメッセージには対応してないよ'
+            msg = 'お前は誰だ'
 
         line_bot_api = MessagingApi(api_client)
         line_bot_api.reply_message_with_http_info(
