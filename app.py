90d518545fa41e0d86a0bf2d10366241c4ded7a4
Ryota Asai <ryota846538@outlook.jp>
2024-05-18 00:34:43 +0900

    :up: add flask-cors

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from models import init_db
 from routes import register_blueprints
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Register the blueprints
 register_blueprints(app)
