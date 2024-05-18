edafc5e05da961868cf3813eb6719b02e3391af4
Ryota Asai <ryota846538@outlook.jp>
2024-05-19 00:42:07 +0900

    :gear: config周り修正

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
+import os
 from dotenv import load_dotenv
 load_dotenv()
-
-import os
 
 YOUR_CHANNEL_SECRET = os.getenv('YOUR_CHANNEL_SECRET')
 YOUR_CHANNEL_ACCESS_TOKEN = os.getenv('YOUR_CHANNEL_ACCESS_TOKEN')
